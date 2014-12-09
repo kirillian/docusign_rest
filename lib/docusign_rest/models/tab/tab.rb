e213f11e911b9654a3fc3fd11a45714e2e61c46e
rdupas <RDupas@secondmarket.com>
2014-12-09 16:39:24 -0500

    override #method_missing on the Tab object

--- lib/docusign_rest/models/tab/tab.rb
+++ lib/docusign_rest/models/tab/tab.rb
@@ -1,5 +1,10 @@
 module DocusignRest
   class Tab < Model
+
+    def method_missing(method, *args, &_)
+      Rails.logger.warn { "#{self.class.name} does not respond to #{method}(#{args})" }
+    end
+
     hash_attr *(%i(
             tabId
             anchorString
