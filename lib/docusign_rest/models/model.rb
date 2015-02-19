67e1e0ed1f17ff0106fb27b6aba3d3b0041dcfa1
Romain Dupas <RDupas@secondmarket.com>
2015-02-19 17:07:54 -0500

    Making gem more error proof

--- lib/docusign_rest/models/model.rb
+++ lib/docusign_rest/models/model.rb
@@ -7,5 +7,9 @@ module DocusignRest
       message = error['message']
       fail message unless message.blank?
     end
+
+    def method_missing(method, *args, &_)
+      Rails.logger.warn { "#{self.class.name} does not respond to #{method}(#{args})" }
+    end
   end
 end
--- lib/docusign_rest/models/tab/tab.rb
+++ lib/docusign_rest/models/tab/tab.rb
@@ -1,10 +1,6 @@
 module DocusignRest
   class Tab < Model
 
-    def method_missing(method, *args, &_)
-      Rails.logger.warn { "#{self.class.name} does not respond to #{method}(#{args})" }
-    end
-
     hash_attr *(%i(
             tabId
             anchorString
