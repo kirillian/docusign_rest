e160240a15cfa837d4b979a3d0ab71ede22c5cb7
Romain Dupas <RDupas@secondmarket.com>
2015-09-24 17:08:40 -0400

    Fix validator

--- lib/docusign_rest/validators/docusign_validator.rb
+++ lib/docusign_rest/validators/docusign_validator.rb
@@ -1,7 +1,5 @@
 class DocusignValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless value.nil? || value.empty?
-      record.errors[attribute] << I18n.t(:invalid_format) unless value =~ /^[a-f0-9-]{36}$/i
-    end
+    record.errors[attribute] << I18n.t(:invalid_format) unless value =~ /^[a-f0-9-]{36}$/i
   end
 end
--- lib/docusign_rest/version.rb
+++ lib/docusign_rest/version.rb
@@ -1,3 +1,3 @@
 module DocusignRest
-  VERSION = '0.2.6'
+  VERSION = '0.2.7'
 end
